4b79f90cd5c39edada1f999bb93656dff213e22b
DiegoBangles <gugu.sm13@gmail.com>
2023-06-05 11:16:31 +0100

    fix

--- main.c
+++ main.c
@@ -65,7 +65,7 @@ void update(STATE *st,MAP *map,POS max,WINDOW *wnd) {
 		case 'p': st->light++; break;
 		case 'l': st->floor++; break;
 		case 'k': st->damage+=10;st->maxhealth+=10;st->health+=10; break;
-		case 'm': wclear(wnd); st->floor=1; break;
+		case 'm': if (st->floor == -1) {wclear(wnd); st->floor=1;} break;
 		case 'z': change_level(map,st,max,wnd); break;
 		case 'x': change_level_final(map,st,max,wnd); break;
 		case 'o': player_death(st,wnd); break;
